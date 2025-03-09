9ea19cd75f8f4881d4d39d1a79fceba19bfbc74e
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2025-03-09 08:43:10 +0300

    add rss fetch function

--- internal/rss/fetcher.go
+++ internal/rss/fetcher.go
@@ -1,7 +1,55 @@
 package internal
 
-import "context"
+import (
+	"context"
+	"encoding/xml"
+	"fmt"
+	"html"
+	"io"
+	"net/http"
+)
 
 func fetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	var feed RSSFeed
 
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, nil)
+	if err != nil {
+		return nil, fmt.Errorf("Error getting request with context: %v", err)
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Error sending request or getting response: %v", err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Unexpected HTTP status: %d", res.StatusCode)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Error reading response body: %v", err)
+	}
+
+	if err := xml.Unmarshal(data, &feed); err != nil {
+		return nil, fmt.Errorf("Error unmarshaling xml data: %v", err)
+	}
+
+	unescapeFeed(&feed)
+
+	return &feed, nil
+}
+
+func unescapeFeed(feed *RSSFeed) {
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+
+	for i, item := range feed.Channel.Item {
+		feed.Channel.Item[i].Title = html.UnescapeString(item.Title)
+		feed.Channel.Item[i].Description = html.UnescapeString(item.Description)
+	}
 }
