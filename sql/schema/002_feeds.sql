da98edc92d59cbb162dc02bd8b085f306aa63e56
AhmettCelik <140803207+AhmettCelik@users.noreply.github.com>
2025-03-10 06:41:59 +0300

    add feeds table goose schema

---
+++ sql/schema/002_feeds.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+CREATE TABLE feeds (
+    id INTEGER PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    name TEXT,
+    url TEXT UNIQUE,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE feeds;
